5174f04e1dba11bbdcd9332c20e8238f1da85534
joalereyesu <61555348+joalereyesu@users.noreply.github.com>
2022-03-29 19:28:37 -0600

    terminado

--- gifs.js
+++ gifs.js
@@ -1,28 +1,99 @@
 const giphyData = {
     api_key: "VqwYQrLEYOi3btJlgOTHADsvnfBZsE5p",
-    limit: 10,
-}
+    limitBegin: 24,
+};
 
-const fetchPromise = fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${giphyData.api_key}&limit=10`);
-let embededTrendigGifs = []
-
+let embededTrendigGifs = [];
+const fetchPromise = fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${giphyData.api_key}&limit=${giphyData.limitBegin}`);
 fetchPromise.then(response => {
     const jsonPromise = response.json();
     jsonPromise.then(json => {
-        let data = json.data;
-        console.log(data)
+        const data = json.data;
         for (let i = 0; i < data.length; i++) {
             embededTrendigGifs.push(data[i]['embed_url']);
         }
-        console.log(embededTrendigGifs);
+        const container = document.getElementById('gifs-container');
+        for (let i = 0; i < embededTrendigGifs.length; i++) {
+            let col = document.createElement('div');
+            col.classList.add('col');
+            let card = document.createElement('div');
+            card.classList.add('card');
+            let frame = document.createElement('iframe');
+            frame.setAttribute('src', embededTrendigGifs[i]);
+            frame.classList.add('giphy-embed');
+            card.appendChild(frame);
+            col.appendChild(card);
+            container.appendChild(col);
+        }
     });
 });
 
 
-// fetchPromise.then(response => {
-//     const jsonPromise = response.json();
-//     jsonPromise.then(json => {
-//         console.log(json.data);
-//     });
-// });
+function loadMore() {
+    randomNumber = Math.floor((Math.random() * 1000) + 1);
+    let embededTrendigGifs = [];
+    const fetchPromise = fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${giphyData.api_key}&limit=9&offset=${randomNumber}`);
+    fetchPromise.then(response => {
+        const jsonPromise = response.json();
+        jsonPromise.then(json => {
+            const data = json.data;
+            for (let i = 0; i < data.length; i++) {
+                embededTrendigGifs.push(data[i]['embed_url']);
+            }
+            const container = document.getElementById('gifs-container');
+            for (let i = 0; i < embededTrendigGifs.length; i++) {
+                let col = document.createElement('div');
+                col.classList.add('col');
+                let card = document.createElement('div');
+                card.classList.add('card');
+                let frame = document.createElement('iframe');
+                frame.setAttribute('src', embededTrendigGifs[i]);
+                frame.classList.add('giphy-embed');
+                card.appendChild(frame);
+                col.appendChild(card);
+                container.appendChild(col);
+            }
+        });
+    });
+}
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function searchGif() {
+    const search = document.getElementById('search').value;
+    console.log(search);
+    const h1 = document.getElementById('heading');
+    h1.innerHTML = "Results for: " + search.toUpperCase();
+    document.getElementById('button-container').remove;
+    let embededTrendigGifs = [];
+    const fetchPromise = fetch(`https://api.giphy.com/v1/gifs/search?api_key=${giphyData.api_key}&q=${search}&limit=${giphyData.limitBegin}`);
+    fetchPromise.then(response => {
+        const jsonPromise = response.json();
+        jsonPromise.then(json => {
+            const data = json.data;
+            console.log(data)
+            for (let i = 0; i < data.length; i++) {
+                embededTrendigGifs.push(data[i]['embed_url']);
+            }
+            console.log(embededTrendigGifs);
+            const container = document.getElementById('gifs-container');
+            removeAllChildNodes(container);
+            for (let i = 0; i < embededTrendigGifs.length; i++) {
+                let col = document.createElement('div');
+                col.classList.add('col');
+                let card = document.createElement('div');
+                card.classList.add('card');
+                let frame = document.createElement('iframe');
+                frame.setAttribute('src', embededTrendigGifs[i]);
+                frame.classList.add('giphy-embed');
+                card.appendChild(frame);
+                col.appendChild(card);
+                container.appendChild(col);
+            }
+        });
+    });
+}
